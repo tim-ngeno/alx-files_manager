fc88ef3c127756bd3fb8f10795b9f3b08e65fc72
Tim Ngeno <timngeno424@gmail.com>
2024-03-11 09:22:28 +0300

    create an express server with endpoints for the API

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+
+const AppController = {
+  async getStatus(req, res) {
+    const redisIsAlive = await redisClient.isAlive();
+    const dbIsAlive = await dbClient.isAlive();
+
+    res.status(200).json({ redis: redisIsAlive, db: dbIsAlive });
+  },
+
+  async getStats(req, res) {
+    const numOfUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
+
+    res.status(200).json({ users: numOfUsers, files: numFiles });
+  },
+};
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
