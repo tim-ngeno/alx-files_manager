1ed257be26d5d898c89eb27bfe4a0e61344b28db
Tim Ngeno <timngeno424@gmail.com>
2024-03-12 16:55:21 +0300

    add enpoint for getFile

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 router.put('/files/:id/publish', FilesController.putPublish);
