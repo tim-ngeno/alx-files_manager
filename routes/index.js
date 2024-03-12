ea7191f68c5df006307c0380378fd8614a75d44d
Tim Ngeno <timngeno424@gmail.com>
2024-03-12 14:16:29 +0300

    rewrite routers

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,12 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-
 router.get('/users/me', UsersController.getMe);
-router.post('/users', UsersController.postNew);
-
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
 module.exports = router;
