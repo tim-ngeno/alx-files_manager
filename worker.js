d6ad613ff35b7cccddcaa8c8fab8317ecdaec8b1
Tim Ngeno <timngeno424@gmail.com>
2024-03-13 10:39:03 +0300

    create worker.js

---
+++ worker.js
@@ -0,0 +1,36 @@
+import Bull from 'bull';
+import sharp from 'sharp';
+import dbClient from './utils/db';
+
+const fileQueue = new Bull('fileQueue');
+
+fileQueue.process(async (job) => {
+  const { userId, fileId } = job.data;
+
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+
+  const mongoClient = await dbClient.getMongoClient();
+  const filesCollection = mongoClient.db().collection('files');
+
+  const file = await filesCollection.findOne({ _id: fileId, userId });
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  // Generate thumbnails
+  const originalFilePath = `${process.env.FOLDER_PATH}/${file.localPath}`;
+
+  // Generate thumbnails of different sizes
+  await Promise.all([
+    sharp(originalFilePath).resize(500).toFile(`${originalFilePath}_500`),
+    sharp(originalFilePath).resize(250).toFile(`${originalFilePath}_250`),
+    sharp(originalFilePath).resize(100).toFile(`${originalFilePath}_100`),
+  ]);
+});
+
+module.exports = fileQueue;
